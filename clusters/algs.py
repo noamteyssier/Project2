e9ab4d03de79f8ba34caf27a63b2a3b301c8b28f
noamteyssier <nb.teyssier@gmail.com>
2021-01-27 13:46:16 -0800

    initial class structuing and comments

--- clusters/algs.py
+++ clusters/algs.py
@@ -1,5 +1,54 @@
-class HierarchicalClustering():
+#!/usr/bin/env python3
+
+class Ligand():
+
+	"""
+	Handles relevant information and tools for ligands
+
+	- IO
+	- SparseArrays
+	- Values
+
+	"""
+
 	pass
 
-class PartitionClustering():
+class Clustering():
+
+	"""
+	Parent class for HierarchicalClustering and PartitionClustering
+	"""
+	pass
+
+
+class HierarchicalClustering(Clustering):
+
+	"""
+	Methods for Hierarchical Clustering
+	"""
+
+	pass
+
+class PartitionClustering(Clustering):
+
+	"""
+	Methods for Hierarchical Clustering
+	"""
+
+	pass
+
+class qCluster():
+
+	"""
+	Evaluates the quality of a given clustering scheme
+	"""
+
+	pass
+
+class simCluster():
+
+	"""
+	Evaluates the similarity between a set of a clusters
+	"""
+
 	pass
