beaeef8ab723212c2098b589a6f112c8524b84b4
noamteyssier <nb.teyssier@gmail.com>
2021-02-10 16:57:32 -0800

    implemented most of kmeans

--- clusters/algs.py
+++ clusters/algs.py
@@ -16,10 +16,13 @@ class Ligand():
 
     """
 
-    def __init__(self, csv):
+    def __init__(self, csv, bitspace=1024):
         self.csv = csv
         self.csv_frame = self._load_csv(csv)
         self.sparse, self.lookup = self._prepare_sparse()
+
+        self.bitspace = bitspace
+        self.mean = self._mean_activation()
 
     def _load_csv(self, csv):
         """
@@ -56,6 +59,22 @@ class Ligand():
         )
 
         return sparse, lookup
+
+    def _mean_activation(self):
+        """
+        finds the mean number of activations across chemical space
+
+        (i.e. the largest value across the sparse vectors)
+        """
+
+        mean_activation = 0
+
+        for s in self.sparse:
+            num_activation = len(self.sparse[s])
+
+            mean_activation += num_activation
+
+        return mean_activation / len(self.sparse)
 
     def PairwiseIter(self):
         """
@@ -179,6 +198,12 @@ class Clustering():
 
                 yield (x, y)
 
+    def _get_cluster_members(self, c):
+        """
+        returns indices of cluster <c> members
+        """
+        return np.flatnonzero(self.labels == c)
+
     def fit(self, **kwargs):
         return self.__fit__(**kwargs)
 
@@ -216,23 +241,17 @@ class HierarchicalClustering(Clustering):
         if verbose:
             print("Merging {} & {} -> {}".format(x, y, z))
 
-        label_indices = np.where((self.labels == x) | (self.labels == y))[0]
+        label_indices = np.flatnonzero((self.labels == x) | (self.labels == y))
         self.labels[label_indices] = z
 
     def _update_linkage(self, x, y, dist, iter):
         """
         update linkage matrix
         """
-        num_orig = np.where(self.labels == x)[0].size + \
-            np.where(self.labels == y)[0].size
+        num_orig = np.flatnonzero(self.labels == x).size + \
+            np.flatnonzero(self.labels == y).size
 
         self.zmat[iter] = np.array([int(x), int(y), dist, int(num_orig)])
-
-    def _get_cluster_members(self, c):
-        """
-        returns indices of cluster <c> members
-        """
-        return np.where(self.labels == c)[0]
 
     def _get_cluster_distances(self, x, y):
         """
@@ -365,7 +384,141 @@ class PartitionClustering(Clustering):
     Methods for K-Means Partition Clustering
     """
 
-    pass
+    def _centroid(self):
+        """
+        initializes a centroid in bitspace
+
+        will initialize the mean number of activations seen in data randomly
+        """
+
+        return set(
+            np.random.randint(
+                low=0,
+                high=self.ligands.bitspace,
+                size=int(self.ligands.mean)
+                )
+            )
+
+    def _member_activations(self, members):
+        """
+        yield all activations found in a cluster
+        """
+        for m in members:
+            for activation in self.ligands.sparse[m]:
+                yield activation
+
+    def _get_activation_frequencies(self, members):
+        """
+        get the frequencies of activations across a cluster membership
+        """
+
+        # initialize empty frequencies
+        frequencies = np.zeros(self.ligands.bitspace)
+
+        # gather all activations seen across members
+        all_activations = np.array([
+            a for a in self._member_activations(members)
+        ])
+
+        # counts of each unique activations in cluster
+        activations, counts = np.unique(all_activations, return_counts=True)
+
+        # assign frequency position (missing activations stay zero)
+        frequencies[activations] = counts
+
+        # convert to probability
+        frequencies = frequencies / frequencies.sum()
+
+        return frequencies
+
+    def _weighted_centroid(self, p):
+        """
+        creates a new centroid with a given multinomial parameterization
+        """
+        mn = np.random.multinomial(self.ligands.bitspace, p)
+        return set(np.flatnonzero(mn))
+
+    def _initialize_centroids(self):
+        """
+        initializes <k> centroids
+        """
+        return [
+            (c, self._centroid()) for c in range(self.k)
+        ]
+
+    def _assign_centroids(self):
+        """
+        assigns each ligand to its closest centroid
+        """
+
+        for lig_idx, lig_bits in self.ligands:
+
+            distances = np.array([
+                self._distance(lig_bits, bits) for idx, bits in self.centroids
+            ])
+
+            assignment = np.random.choice(
+                np.flatnonzero(distances == distances.max())
+                )
+
+            self.labels[lig_idx] = assignment
+
+    def _intercluster_distance(self, k):
+        """
+        sum pairwise distances within a cluster <k>
+        """
+
+        clust = self._get_cluster_members(k)
+        distances = np.array([
+            self._get_distance(x, y) for x, y in self._pairwise_iter(clust)
+        ])
+
+        return distances.sum()
+
+    def _evaluate_clusters(self):
+        """
+        sums inter-cluster distances across all clusters
+        """
+
+        cluster_distances = np.array([
+            self._intercluster_distance(k) for k in range(self.k)
+        ])
+
+        return cluster_distances.sum()
+
+    def _update_centroids(self):
+        """
+        updates centroids to reflect members
+        """
+
+        for k in range(self.k):
+
+            # retrieve members of a cluster
+            members = self._get_cluster_members(k)
+
+            # calculate activation frequencies
+            frequencies = self._get_activation_frequencies(members)
+
+            # build centroid with multinomial derived activations
+            centroid = self._weighted_centroid(frequencies)
+
+            print(len(centroid))
+
+            break
+        pass
+
+    def __fit__(self, k, seed=42, epsilon=1e-2):
+        np.random.seed(seed)
+        self.k = k
+        self.centroids = self._initialize_centroids()
+        self.labels = np.zeros(len(self.ligands), dtype=int)
+
+        while True:
+
+            self._assign_centroids()
+            self._evaluate_clusters()
+            self._update_centroids()
+            break
 
 
 class qCluster():
@@ -388,8 +541,11 @@ class simCluster():
 
 def main():
     ligand_obj = Ligand("../data/test_set.csv")
-    hcl = HierarchicalClustering(ligand_obj)
-    hcl.fit(verbose=True, linkage="single")
+    # hcl = HierarchicalClustering(ligand_obj)
+    # hcl.fit(verbose=True, linkage="single")
+
+    pcl = PartitionClustering(ligand_obj)
+    pcl.fit(k=5)
 
     pass
 
